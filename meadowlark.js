8034107e88a4f69bc84ac0ad2351aabf0f08c70f
jiedai <jiedai@amazon.com>
2017-08-04 19:17:54 +0800

    Handling forms with AJAX

--- meadowlark.js
+++ meadowlark.js
@@ -4,38 +4,74 @@ var fortune = require('./lib/fortune');
 var app = express();
 
 // setup handlebars view engine
-var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main',
+    helpers: {
+        section: function(name, options) {
+            if (!this._sections) {
+                this._sections = {};
+            }
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
+// Add static files
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
+app.use(require('body-parser').urlencoded({
+    extended: true
+}));
+
+app.get('/', function (req, res) {
     // res.type('text/plain');
     // res.send('Meadowlark Travel');
     res.render('home');
 });
 
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
     res.render('about', {
         fortune: fortune.getFortune()
     });
 });
 
-app.use(function(req, res) {
+app.get('/newsletter', function (req, res) {
+    res.render('newsletter', {
+        csrf: 'CSRF token goes here'
+    });
+});
+
+app.post('/process', function(req, res) {
+    console.log('Form (form querystring):' + req.query.form);
+    console.log('CSRF token (from hidden form field):' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+
+    if (req.xhr || req.accepts('json, html') === 'json') {
+        res.send({success: true});
+    }
+    else {
+        res.redirect(303, '/thank-you');
+    }
+});
+
+app.use(function (req, res) {
     res.status(404);
     res.render('404');
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render('500');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost' + app.get('port') +
         '; press Ctrl-C to terminate');
 })
